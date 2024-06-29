a42c3b0c3a63c7a0e9bf861dc0db0bfdfc405445
Victor <victorgladys@gmail.com>
2024-06-29 16:45:41 +0200

    WIP on Classic_models

--- ClassicalModels/Model3.py
+++ ClassicalModels/Model3.py
@@ -8,25 +8,35 @@ import numpy as np
 from components.parametric_graph_filter import ParametricGraphFilter
 from components.space_time_pooling import SpaceTimeMaxPooling
 from torch_geometric.nn import GCNConv
+from preprocessor_final_data import Preprocessor
+from preprocessor_final_data import draw_network
+from preprocessor_final_data import get_adj_from_plot
 
 
 
 class GraphConvolutionalNetwork(nn.Module):
-    def __init__(self, num_params, input_horizon, prediction_horizon, num_features, input_dim, output_dim, num_nodes_kron, num_nodes_pred):
+    def __init__(self, num_params, input_horizon, prediction_horizon, preproc):
         super(GraphConvolutionalNetwork, self).__init__()
+        
+        self.features = ['weather1', 'weather2', 'cumulative_confirmed', 'cumulative_deceased', 'new_deceased', 'cumulative_persons_fully_vaccinated', 'new_persons_fully_vaccinated']
+        self.training_data, self.testing_data = self.create_dataset_for_dataloader(preproc, input_horizon, prediction_horizon)
+        
+        self.num_nodes_kron = self.training_data[0][0].shape[0]
+        self.num_nodes_pred = len(self.training_data[0][2])
+
+        self.num_features = len(list(self.train_graph_sig.values())[0])
+        self.input_dim = self.num_features  # Number of features per node
+        self.output_dim = self.num_features  # Number of output features per node
+        
         self.hidden_dim_low, self.hidden_dim_high = get_parameters(num_params)
-        self.mygconv1 = GCNConv(input_dim, self.hidden_dim_low)
+        self.mygconv1 = GCNConv(self.input_dim, self.hidden_dim_low)
         self.gconv2 = GCNConv(self.hidden_dim_low, self.hidden_dim_low)
         # same MLP trained per node, just declare it here
         self.MLP = nn.Sequential(
             nn.Linear(self.hidden_dim_low, self.hidden_dim_high), 
             nn.ReLU(),
-            nn.Linear(self.hidden_dim_high, output_dim) 
+            nn.Linear(self.hidden_dim_high, self.output_dim) 
         )
-        self.num_features = num_features
-        self.num_nodes_kron = num_nodes_kron
-        self.num_nodes_pred = num_nodes_pred
-        self.features = ['weather1', 'weather2', 'cumulative_confirmed', 'cumulative_deceased', 'new_deceased', 'cumulative_persons_fully_vaccinated', 'new_persons_fully_vaccinated']
 
     
     def forward(self, data):
@@ -45,20 +55,11 @@ class GraphConvolutionalNetwork(nn.Module):
         x = self.MLP(x)
         return x
     
-    def preprocess(self, flow_dataset, epi_dataset, locations_data, plottable=True):
-        preproc = preprocessor_final_data.Preprocessor(flow_dataset, epi_dataset, locations_data, plottable=True)
-        graph_kronecker_whole_df = preproc.combined_manual_kronecker() # makes the pandas df from the kronecker data
-        num_nodes_per_day = len(list_of_geoids)
-        adj_kronecker_whole = get_adj_from_plot(graph_kronecker_whole_df)
-
-        # Convert adjacency to edge_index
-        adj_as_edge_index = torch.tensor(adjacency.nonzero(), dtype=torch.long)
-    
     def process_input(self, data):
         adjacency, graph_signal, target_graph_signal = data
         
         # Ensure graph_signal should have shape [number_of_nodes_kron, input_dim]
-        self.graph_signal_matrix = np.zeros((self.num_nodes_kron, self.num_features)) # the graph signal matrix will change per training example
+        self.graph_signal_matrix = np.zeros((self.num_nodes_kron, self.num_features))  # the graph signal matrix will change per training example
 
         for cur_node, element in enumerate(list(graph_signal.values())):
             for i in range(self.num_features):
@@ -80,6 +81,139 @@ class GraphConvolutionalNetwork(nn.Module):
 
     def getLoss(self, output):
         return nn.MSELoss()(output, self.target_graph_signal_matrix)
+
+    def create_dataset_for_dataloader(self, preproc, input_hor, pred_hor, train_perc = 0.8, test_perc = 0.2):
+        print("Getting the training graph signal...")
+
+        # Get the kronecker
+        self.graph_kronecker_whole_df = preproc.combined_manual_kronecker() # makes the pandas df from the kronecker data
+        adj_kronecker_whole = get_adj_from_plot(self.graph_kronecker_whole_df)
+        
+        # do it first only with cumulative_confirmed
+        tr_epi = preproc.set_timestep_offset_epi_dataset(from_timestep=0).get_epi_dataset() # will get the epi info for the entire dataset, the entire kronecker, then later we index per training example and pred
+        
+        self.train_graph_sig = {}
+        for day in tr_epi:
+            entry_count = 0
+            for geoid in day.geoid_o:
+                if geoid not in self.train_graph_sig: # which should be for every day
+                    self.train_graph_sig[geoid] = {
+                        'weather1': [],
+                        'weather2': [],
+                        'cumulative_confirmed': [],
+                        'cumulative_deceased': [],
+                        'new_deceased': [],
+                        'cumulative_persons_fully_vaccinated': [],
+                        'new_persons_fully_vaccinated': []
+                    }
+        
+                self.train_graph_sig[geoid]['weather1'].append(day.weather1[entry_count])
+                self.train_graph_sig[geoid]['weather2'].append(day.weather2[entry_count])
+                self.train_graph_sig[geoid]['cumulative_confirmed'].append(day.cumulative_confirmed[entry_count])
+                self.train_graph_sig[geoid]['cumulative_deceased'].append(day.cumulative_deceased[entry_count])
+                self.train_graph_sig[geoid]['new_deceased'].append(day.new_deceased[entry_count])
+                self.train_graph_sig[geoid]['cumulative_persons_fully_vaccinated'].append(day.cumulative_persons_fully_vaccinated[entry_count])
+                self.train_graph_sig[geoid]['new_persons_fully_vaccinated'].append(day.new_persons_fully_vaccinated[entry_count])
+                entry_count += 1
+                
+        # sort train_graph_sig by geoid_o
+        self.train_graph_sig = dict(sorted(self.train_graph_sig.items(), key=lambda item: item[0]))
+    
+        # Combine all values for each feature across all geo_ids
+        weather1_values = []
+        weather2_values = []
+        cumulative_confirmed_values = []
+        cumulative_deceased_values = []
+        new_deceased_values = []
+        cumulative_persons_fully_vaccinated_values = []
+        new_persons_fully_vaccinated_values = []
+        
+        for geoid in self.train_graph_sig:
+            weather1_values.extend(self.train_graph_sig[geoid]['weather1'])
+            weather2_values.extend(self.train_graph_sig[geoid]['weather2'])
+            cumulative_confirmed_values.extend(self.train_graph_sig[geoid]['cumulative_confirmed'])
+            cumulative_deceased_values.extend(self.train_graph_sig[geoid]['cumulative_deceased'])
+            new_deceased_values.extend(self.train_graph_sig[geoid]['new_deceased'])
+            cumulative_persons_fully_vaccinated_values.extend(self.train_graph_sig[geoid]['cumulative_persons_fully_vaccinated'])
+            new_persons_fully_vaccinated_values.extend(self.train_graph_sig[geoid]['new_persons_fully_vaccinated'])
+        
+        # Calculate mean and std for each feature across all geo_ids
+        mean_weather1 = np.mean(weather1_values)
+        std_weather1 = np.std(weather1_values)
+        
+        mean_weather2 = np.mean(weather2_values)
+        std_weather2 = np.std(weather2_values)
+        
+        mean_cumulative_confirmed = np.mean(cumulative_confirmed_values)
+        std_cumulative_confirmed = np.std(cumulative_confirmed_values)
+        
+        mean_cumulative_deceased = np.mean(cumulative_deceased_values)
+        std_cumulative_deceased = np.std(cumulative_deceased_values)
+        
+        mean_new_deceased = np.mean(new_deceased_values)
+        std_new_deceased = np.std(new_deceased_values)
+        
+        mean_cumulative_persons_fully_vaccinated = np.mean(cumulative_persons_fully_vaccinated_values)
+        std_cumulative_persons_fully_vaccinated = np.std(cumulative_persons_fully_vaccinated_values)
+        
+        mean_new_persons_fully_vaccinated = np.mean(new_persons_fully_vaccinated_values)
+        std_new_persons_fully_vaccinated = np.std(new_persons_fully_vaccinated_values)
+        
+        # Normalize each feature list in train_graph_sig across all geo_ids
+        for geoid in self.train_graph_sig:
+            self.train_graph_sig[geoid]['weather1'] = (self.train_graph_sig[geoid]['weather1'] - mean_weather1) / std_weather1 if std_weather1 != 0 else 0
+            self.train_graph_sig[geoid]['weather2'] = (self.train_graph_sig[geoid]['weather2'] - mean_weather2) / std_weather2 if std_weather2 != 0 else 0
+            self.train_graph_sig[geoid]['cumulative_confirmed'] = (self.train_graph_sig[geoid]['cumulative_confirmed'] - mean_cumulative_confirmed) / std_cumulative_confirmed if std_cumulative_confirmed != 0 else 0
+            self.train_graph_sig[geoid]['cumulative_deceased'] = (self.train_graph_sig[geoid]['cumulative_deceased'] - mean_cumulative_deceased) / std_cumulative_deceased if std_cumulative_deceased != 0 else 0
+            self.train_graph_sig[geoid]['new_deceased'] = (self.train_graph_sig[geoid]['new_deceased'] - mean_new_deceased) / std_new_deceased if std_new_deceased != 0 else 0
+            self.train_graph_sig[geoid]['cumulative_persons_fully_vaccinated'] = (self.train_graph_sig[geoid]['cumulative_persons_fully_vaccinated'] - mean_cumulative_persons_fully_vaccinated) / std_cumulative_persons_fully_vaccinated if std_cumulative_persons_fully_vaccinated != 0 else 0
+            self.train_graph_sig[geoid]['new_persons_fully_vaccinated'] = (self.train_graph_sig[geoid]['new_persons_fully_vaccinated'] - mean_new_persons_fully_vaccinated) / std_new_persons_fully_vaccinated if std_new_persons_fully_vaccinated != 0 else 0
+    
+        all_examples = []
+        
+        list_of_geoids = preproc.flow.iloc[:]['geoid_o'].unique()
+        num_nodes_per_day = len(list_of_geoids)
+
+        print(len(tr_epi), input_hor)
+        for example_num in range(0, len(tr_epi) - (input_hor)): # this is how many times you can "shift and have valid data to pull from"
+            print("was here")
+            # now draw out the adjacency matrix per example
+            width_of_adj_per_example = num_nodes_per_day*input_hor
+            shift = num_nodes_per_day # number of nodes per day
+        
+            offset = (example_num * shift)
+            # this should still work in the new approach
+            adj_per_example = adj_kronecker_whole[ offset : offset + width_of_adj_per_example,
+                                                   offset : offset + width_of_adj_per_example]
+            print(adj_kronecker_whole.shape)
+            print(f"numnnodes p day: {num_nodes_per_day} input hor: {input_hor} offset: {offset}")
+            print(adj_per_example.shape)
+            # now drawing out the train_graph_signal per example
+        
+            # get the graph signal corresponding to the example nodes
+            train_graph_sig_per_example = {k: self.train_graph_sig[k] for k in \
+                                           list(self.train_graph_sig)[offset : offset + width_of_adj_per_example]}
+        
+            # get the graph signal corresponding to the [input_hor : input_hor+pred_hor] set of nodes
+            train_graph_sig_per_example_pred = {k: self.train_graph_sig[k] for k in \
+                                                list(self.train_graph_sig)[offset + (num_nodes_per_day * input_hor) : offset + num_nodes_per_day*(input_hor + pred_hor)]}
+        
+            training_example = [adj_per_example, train_graph_sig_per_example, train_graph_sig_per_example_pred]
+            all_examples.append(training_example)
+        
+        # remove those examples from all_training_examples
+        all_training_examples = all_examples[:int(len(all_examples)*train_perc)]
+        # take the last 20% of the the training examples and use them as test examples
+        all_test_examples = all_examples[int(len(all_examples)*train_perc):]
+            
+        print(all_training_examples[0][0].shape[0])
+        adjacency, graph_signal, target_graph_signal = all_training_examples[0]
+        num_nodes_kron = all_training_examples[0][0].shape[0]
+        graph_signal = len(all_training_examples[0][1].values())
+        print(num_nodes_kron)
+        print(graph_signal)
+        return all_training_examples, all_test_examples
+
 
 def get_parameters(desired_params):
     mlp_params = round_up_to_nearest_hundred(desired_params*(2/3))
