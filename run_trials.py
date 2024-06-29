fixed GCNN RNN and added some fix to edgeweight normalisation

run_trials.py
@@ -34,7 +34,8 @@ def train(config=None):
             "n_features": 1,
             "profile": False,
             "min_delta": 0,
-            "mlp_width": config.rel_mlp_width
+            "mlp_width": config.rel_mlp_width,
+            "dev_run": False,
 
         }
         
@@ -69,7 +70,7 @@ def train(config=None):
         elif config["model"] == "GCNN_RNN":
             model = GCNN_RNNModule(config_dict, fixed_edge_weights=fixed_edge_weights)
         elif config["model"] == "TCN":
-            model = TCNModule(config_dict)
+            raise NotImplementedError("TCN not implemented")
         else:
             raise NotImplementedError(f"Model {config['model']} not implemented.")
         
@@ -83,7 +84,8 @@ def train(config=None):
             max_epochs=config_dict["n_epochs"],
             accelerator="gpu" if torch.cuda.is_available() else "cpu",
             devices=1 if torch.cuda.is_available() else None,
-            log_every_n_steps=1
+            log_every_n_steps=1,
+            fast_dev_run=config_dict["dev_run"],
         )
         
         trainer.fit(model, datamodule=data_module)

src/lightning_modules/model_modules/GCNN_RNN_module.py
@@ -40,7 +40,7 @@ class GCNN_RNNModule(pl.LightningModule):
     
     def criterion(self, output, target):
         MSE = torch.nn.MSELoss()
-        return MSE(output[:, -self.config["pred_hor"]:, :, :], target)
+        return MSE(output[:, -self.config["pred_hor"]:, :, -self.config["pred_hor"]:], target)
     
     def forward(self, x_in, edge_weights=None):
 

src/models/GCNN_RNN/GCNN_RNN.py
@@ -16,10 +16,15 @@ class GCNN(torch.nn.Module):
         self.graph_convolution = pyg.nn.GCNConv(n_features, n_output_features)
         
     def forward(self, x, edge_idx, edge_weights=None):
+        """ x: torch.Tensor of shape (batch,n_nodes, n_features)
+            edge_idx: torch.Tensor of shape (batch, 2, n_edges)
+            edge_weights: torch.Tensor of shape (batch, n_edges)"""
         x_out = torch.zeros(x.shape[0], self.n_nodes, self.n_output_features, device= self.device, dtype= self.dtype)
+
         
-        for i in range(x.shape[0]):            
-            x_out[i, :, :] = self.graph_convolution(x[i, :, :], edge_idx[i, :, :], edge_weights[i, :])
+        for i in range(x.shape[0]):  
+            out_GCN_conv = self.graph_convolution(x[i, :, :], edge_idx[i, :, :], edge_weights[i, :])
+            x_out[i, :, :] = out_GCN_conv
         return x_out
 """
 needs to be imported like this:
@@ -51,15 +56,33 @@ class GCNN_RNN(torch.nn.Module):
                          n_output_features= n_out_features, device= device, 
                          dtype= dtype, fixed_edge_weights= fixed_edge_weights)
         self.GCNN.to(device)
-        self.RNN = torch.nn.RNN(input_size=n_out_features, hidden_size=h_size,  batch_first=True, device=device, dtype=dtype)
+        self.RNN = torch.nn.RNN(input_size=n_out_features+n_features, hidden_size=h_size,  batch_first=True, device=device, dtype=dtype)
         self.MLP = torch.nn.Sequential(
             torch.nn.Linear(h_size, h_size*mlp_width),
             torch.nn.ReLU(),
             torch.nn.Linear(h_size*mlp_width, h_size*mlp_width),
             torch.nn.ReLU(),
-            torch.nn.Linear(h_size*mlp_width, n_out_features)
-            
+            torch.nn.Linear(h_size*mlp_width, n_features+n_out_features)
         )
+        
+        self.fixed_edge_weights = self.fixed_edge_weights.transpose(0, 1)
+        
+        self.fixed_edge_idx = self.fixed_edge_weights[:2, :].type(torch.int32)
+        self.fixed_edge_weights = self.fixed_edge_weights[2, :]
+        self.fixed_edge_idx.to(self.device)
+        self.fixed_edge_weights.to(self.device)
+
+        # Extracting node IDs and creating a mapping from IDs to indices
+        unique_id = self.fixed_edge_idx[:,:].unique()
+        unique_id.sort()
+        print("unique_id: ", unique_id)
+        map_id = {j.item(): i for i, j in enumerate(unique_id)}
+        # Processing edge Tensor: replacing node IDs with corresponding indices
+        
+        for j in range(self.fixed_edge_idx.shape[1]):
+            self.fixed_edge_idx[0, j] = map_id[self.fixed_edge_idx[0, j].item()]
+            self.fixed_edge_idx[1, j] = map_id[self.fixed_edge_idx[1, j].item()]
+        
     def forward(self, x_in, edge_weights=None, pred_hor = 1):
         if edge_weights is not None:
             raise ValueError("Only fixed edge weights are supported")
@@ -69,53 +92,52 @@ class GCNN_RNN(torch.nn.Module):
         
         x_in = x_in.view(-1, self.n_nodes, self.n_features)
         x_in.to(self.device)
-        
-        fixed_edge_weights = self.fixed_edge_weights.transpose(0, 1)
-        
-        self.dup_fixed_edge_weights = fixed_edge_weights.unsqueeze(0).expand(batch_size*self.input_horizon, -1, -1)
-        self.dup_fixed_edge_idx = self.dup_fixed_edge_weights[:, :2, :].type(torch.int32)
-        self.dup_fixed_edge_weights = self.dup_fixed_edge_weights[:, 2, :]
-        self.dup_fixed_edge_idx.to(self.device)
-        self.dup_fixed_edge_weights.to(self.device)
-        
-        # Extracting node IDs and creating a mapping from IDs to indices
-        unique_id = self.dup_fixed_edge_idx[:,:,:].unique()
-        map_id = {j.item(): i for i, j in enumerate(unique_id)}
+        self.dup_fixed_edge_idx = self.fixed_edge_idx.unsqueeze(0).expand(batch_size*self.input_horizon, -1, -1)
+        self.dup_fixed_edge_weights = self.fixed_edge_weights.unsqueeze(0).expand(batch_size*self.input_horizon, -1)
 
-        # Processing edge Tensor: replacing node IDs with corresponding indices
-        for i, batch in enumerate(self.dup_fixed_edge_idx):
-            for j, _ in enumerate(batch[0]):
-                self.dup_fixed_edge_idx[i, 0, j] = map_id[self.dup_fixed_edge_idx[i, 0, j].item()]
-                self.dup_fixed_edge_idx[i, 1, j] = map_id[self.dup_fixed_edge_idx[i, 1, j].item()]
+        x_out_of_GCNN = self.GCNN(x_in, edge_idx = self.dup_fixed_edge_idx, edge_weights= self.dup_fixed_edge_weights)
         
-        x = self.GCNN(x_in, edge_idx = self.dup_fixed_edge_idx, edge_weights= self.dup_fixed_edge_weights)
-        
+
         # I reduce the input horizon by 2, because otherwise the size of x doesn't factor out to these four variables
-        x = x.view(batch_size * self.n_nodes, self.input_horizon, self.n_out_features)
+        x_out_of_GCNN = x_out_of_GCNN.view(batch_size * self.n_nodes, self.input_horizon, self.n_out_features)
         
+        x_into_RNN = torch.zeros((batch_size * self.n_nodes, self.input_horizon, self.n_out_features+self.n_features), device=self.device)
+        x_into_RNN[:, :, :self.n_features] = x_in.view(batch_size * self.n_nodes, self.input_horizon, self.n_features)
+        x_into_RNN[:, :, self.n_features:] = x_out_of_GCNN
         #change 1 to n_layers
         h = torch.zeros((1, batch_size * self.n_nodes, self.h_size), device=self.device)
         
-        print(" X shape: ", x.shape)
+        print("x_into_RNN[0, :, :]: ", x_into_RNN[0, :, :])
+        print(" X into RNN shape: ", x_into_RNN.shape)
         print("H shape: ", h.shape)
         
-        x_out_final = torch.zeros((batch_size * self.n_nodes, self.input_horizon + self.prediction_horizon, self.n_out_features), device=self.device)
-        RNN_out, h_out = self.RNN(x, h)
-        print("x_out.shape) ", RNN_out.shape)
+        x_out_final = torch.zeros((batch_size * self.n_nodes, self.input_horizon + self.prediction_horizon, self.n_out_features + self.n_features), device=self.device)
+        
+        RNN_out, h_out = self.RNN(x_into_RNN, h)
+        
+        print("RNN_out[0, :, 0] ", RNN_out[0, :, 0])
+        print("h_out[0, :, 0] ", h_out[0, :, 0])
+        print("RNN_out.shape) ", RNN_out.shape)
         print("h_out.shape) ", h_out.shape)
         
         for i in range(self.input_horizon):
+            print("RNN_out[:, i, :].shape: ", RNN_out[:, i, :].shape)
             x_out_final[:, i, :] = self.MLP(RNN_out[:, i, :])
+            print("x_out_final[0, i, :5]: ", x_out_final[0, i, :5])
             
-        if pred_hor > 1:
-            for i in range(pred_hor-1):
-                RNN_out, h_out = self.RNN(x_out_final[:,self.input_horizon+i, :], h_out[-1, :, :])
+        
+        for i in range(pred_hor):
+            print("h_out.shape: ", h_out.shape)
+            input_fed_back = x_out_final[:,self.input_horizon+i, :].clone().view(batch_size*self.n_nodes, 1,  self.n_out_features + self.n_features)
+            RNN_out, h_out = self.RNN(input_fed_back, h_out)
+            print("RNN_out.shape: ", RNN_out.shape)
+            print("h_out.shape: ", h_out.shape)
+            x_out_final[:, self.input_horizon + i, :] = self.MLP(RNN_out[:, -1, :])
                 
-                x_out_final[:, self.input_horizon + i, :] = self.MLP(RNN_out[:, :, :])
         
         print(x_out_final.shape)        
         print(x_out_final[0, :, :5])
-        x_out_final = x_out_final.view(batch_size, self.input_horizon + self.prediction_horizon, self.n_nodes, self.n_out_features)
+        x_out_final = x_out_final.view(batch_size, self.input_horizon + self.prediction_horizon, self.n_nodes, self.n_out_features + self.n_features)
         print(x_out_final.shape)
         return x_out_final
     

src/utils/data_management/Split_graph_data.py
@@ -55,9 +55,13 @@ class Split_graph_dataset(torch.utils.data.Dataset):
             mean_edge_weight = self.edge_weights[:, :, 2].mean().item()
             std_dev_edge_weight = self.edge_weights[:, :, 2].std().item()
             self.edge_weights[:, :, 2] = (self.edge_weights[:, :, 2] - mean_edge_weight) / std_dev_edge_weight
+            min_edge_weight = self.edge_weights[:, :, 2].min().item()
+            self.edge_weights[:, :, 2] = self.edge_weights[:, :, 2] - min_edge_weight
         else:
-            max_edge_weight = self.edge_weights[:, :, 2].max().item()
+            max_edge_weight = self.edge_weights[:, :, 2].abs().max().item()
             self.edge_weights[:, :, 2] = self.edge_weights[:, :, 2] / max_edge_weight
+            min_edge_weight = self.edge_weights[:, :, 2].min().item()
+            self.edge_weights[:, :, 2] = self.edge_weights[:, :, 2] - min_edge_weight
 
         for feature in range(self.n_features):
             mean_node_data = self.node_data[:, :, feature].mean().item()